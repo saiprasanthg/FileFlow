754e013418a0d08c74bdb6e2647d1da3d3c2aecd
svdpatibandla <113794074+svdpatibandla@users.noreply.github.com>
2023-04-25 15:51:13 -0700

    Delete packages/mobile directory

--- packages/mobile/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { YellowBox } from "react-native";
-import Routes from "./routes";
-
-YellowBox.ignoreWarnings(["Unrecognized WebSocket"]);
-
-const App = () => <Routes />;
-
-export default App;
